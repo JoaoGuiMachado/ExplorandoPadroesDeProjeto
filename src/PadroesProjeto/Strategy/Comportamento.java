ea6d04fda15dd54e35c1ccf5d4d5b405f979f32d
JoaoGuiMachado <joao.gmachado14@gmail.com>
2023-04-24 10:35:06 -0300

    Padrões de Projetos

---
+++ src/PadroesProjeto/Strategy/Comportamento.java
@@ -0,0 +1,7 @@
+package PadroesProjeto.Strategy;
+
+public interface Comportamento {
+    void mover();
+
+
+}
---
+++ src/PadroesProjeto/Strategy/ComportamentoAgressivo.java
@@ -0,0 +1,9 @@
+package PadroesProjeto.Strategy;
+
+public class ComportamentoAgressivo implements Comportamento{
+    
+    public void mover(){
+        System.out.println("Movendo-se agressivamente....");
+    }
+        
+}
